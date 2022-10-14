f05495a8ed8c144045b587739ec0bc12f0cd588e
olakrhoff <ola.kr@hotmail.no>
2022-10-14 10:59:27 +0200

    Feat: task3 sudocode added

---
+++ src/task3.py
@@ -0,0 +1,17 @@
+
+# Loop for s in S
+#   add s to suffix tree
+#   keep track of sequences through nodes
+# Traverse suffix tree by going to child node with at least 95% of sequences visited
+# if no option, stop, return current
+
+# Other method for doing this
+# Loop for s in S
+#   add s to suffix tree
+# Find greatest common substring
+
+
+
+
+if __name__ == '__main__':
+    print('hi')
